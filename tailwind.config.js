ef9a6a5d320613a4aaf4c62c06c1eed61d8b7e57
Czarabrar <abrarahmedpbt53@gmail.com>
2024-06-07 12:02:22 +0530

    deleted

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-   content: ["./App.{js,jsx,ts,tsx}", "./Source/Screens/**/*.{js,jsx,ts,tsx}","./Source/Components/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
